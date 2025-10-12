dcb0842bd4080c6763c58070923ade1bd265c35a
hvozdetskyi <denishwosdetskyi@gmail.com>
2025-10-12 12:40:52 +0200

    Method clean and placeToken in the class Board implemented, v1.0

--- src/main/java/edu/io/Board.java
+++ src/main/java/edu/io/Board.java
@@ -7,6 +7,21 @@ public class Board {
     public Board(int size) {
         this.size = size;
         this.grid = new Token[size][size];
+        clean();
+    }
+
+    public void clean() {
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                grid[row][col] = new Token("・");
+            }
+        }
+    }
+
+    public void placeToken(int col, int row, Token token) {
+        if (row >= 0 && row < size && col >= 0 && col < size) {
+            grid[row][col] = token;
+        }
     }
 }
 
