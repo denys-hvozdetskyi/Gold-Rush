a9cf80dce57c089fe28297808fc7196f334f282e
hvozdetskyi <denishwosdetskyi@gmail.com>
2025-10-12 12:30:54 +0200

    Class Board added

--- src/main/java/edu/io/Board.java
+++ src/main/java/edu/io/Board.java
@@ -1,8 +1,11 @@
 package edu.io;
 
 public class Board {
+    private int size;
+    private Token[][] grid;
 
-    public Board() {
+    public Board(int size) {
+        this.size = size;
+        this.grid = new Token[size][size];
     }
-
 }
