868e6ea851f853fb74b8a8198604f3452f01c8bc
hvozdetskyi <denishwosdetskyi@gmail.com>
2025-10-13 20:58:31 +0200

    All the Board class methods implemented, local test in class Main - success, v1.1

--- src/main/java/edu/io/Board.java
+++ src/main/java/edu/io/Board.java
@@ -10,6 +10,7 @@ public class Board {
         clean();
     }
 
+    // Reset the board
     public void clean() {
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
@@ -18,9 +19,28 @@ public class Board {
         }
     }
 
+    // Place Token on Board
     public void placeToken(int col, int row, Token token) {
         if (row >= 0 && row < size && col >= 0 && col < size) {
             grid[row][col] = token;
+        }
+    }
+
+    // Returns the Token located on coordinates from parameters
+    public Token square(int col, int row) {
+        if (row >= 0 && row < size && col >= 0 && col < size) {
+            return grid[row][col];
+        }
+        return null;
+    }
+
+    // Display the Token
+    public void display() {
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                System.out.print(grid[row][col].label + " ");
+            }
+            System.out.println();
         }
     }
 }
--- src/main/java/edu/io/Main.java
+++ src/main/java/edu/io/Main.java
@@ -3,5 +3,16 @@ package edu.io;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Gold Rush");
+
+        Board board = new Board(8);
+
+        Token player = new Token("웃");
+        Token gold = new Token("\uD83D\uDCB0");
+
+        board.clean();
+        board.placeToken(7, 7, player);
+        board.placeToken(0, 0, gold);
+
+        board.display();
     }
 }
